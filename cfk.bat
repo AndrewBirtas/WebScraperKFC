0bdc0ed6447d612bb499589da889f5e561817707
AndrewBirtas <71767826+AndrewBirtas@users.noreply.github.com>
2020-09-23 15:34:47 +0300

    Update cfk.bat

--- cfk.bat
+++ cfk.bat
@@ -1,1 +1,2 @@
-@py.exe "C:\Users\birta\OneDrive\Desktop\cfk.py" %*
+@py.exe "C:\Users\birta\OneDrive\Desktop\cfk.py" %*  
+:: With this program we can add the cfk.py to the scheduler in windows and make it run in on a fixed schedule.
