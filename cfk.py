62308920b2e5736be287ce6d416e1eb1e6330a13
AndrewBirtas <71767826+AndrewBirtas@users.noreply.github.com>
2020-09-23 15:32:27 +0300

    Update cfk.py

--- cfk.py
+++ cfk.py
@@ -6,14 +6,14 @@ getPage = requests.get("https://studycoin.kfc.ro/")
 getPage.raise_for_status()
 
 
-text = bs4.BeautifulSoup(getPage.text, 'html.parser')
-iteme = text.select('.tickerItem')
+text = bs4.BeautifulSoup(getPage.text, 'html.parser') #get html
+iteme = text.select('.tickerItem') #get desired item class from html
 
 
 what_i_want = '1 studycoin'
 flength = len(what_i_want)
 
-
+#search for the exact item
 for x in iteme:
     for i in range(len(x.text)):
         chunk = x.text[i:i+flength].lower()
@@ -24,6 +24,6 @@ for x in iteme:
 
 print(switch)
 if switch == True:
-    os.system("start cmd")
+    os.system("start cmd") #do something if the value is true!
 print(value)
  	
